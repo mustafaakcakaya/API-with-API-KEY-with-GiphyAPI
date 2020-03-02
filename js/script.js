d232e25c351aab1addb32761f1231de3306f365c
Mustafa AKÇAKAYA <mustafaakcakaya@users.noreply.github.com>
2020-03-02 23:43:19 +0300

    we added tag to constrain API data url. Started checking to add a gif that comes from API  to site .

--- js/script.js
+++ js/script.js
@@ -1,12 +1,18 @@
 var request = new XMLHttpRequest();
 
-request.open('GET', 'http://api.giphy.com/v1/gifs/random?api_key=z9qEJKCAcrrMCA7q77qHJXLDkv111Kra');
+request.open('GET', 'http://api.giphy.com/v1/gifs/random?api_key=z9qEJKCAcrrMCA7q77qHJXLDkv111Kra&tag=squirrel&rating=g');
 
 request.onload = function () {
     var response = request.response;
     var parsedData = JSON.parse(response);
-
     console.log(parsedData);
+    var originalUrl = parsedData.data.url;
+    console.log(originalUrl);
+
+
+    var gif = document.createElement('img');
+    gif.setAttribute('src', originalUrl);
+    document.body.appendChild(gif);
 
 }
 
